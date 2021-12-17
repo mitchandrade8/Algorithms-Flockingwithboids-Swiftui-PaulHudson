6f1055d0045bda7d11694ad2a26e235669365700
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2021-12-17 11:06:01 -0700

    Initial Commit

---
+++ Flockingwithboids/FlockingwithboidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlockingwithboidsApp.swift
+//  Flockingwithboids
+//
+//  Created by Mitch Andrade on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FlockingwithboidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
